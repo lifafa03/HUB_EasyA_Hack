df6431b86b37b284ccbc35d745ad96d1bd05eb62
Evan <evan.dickinson.flinn@gmail.com>
2025-04-27 00:13:43 -0400

    added needed updates to smart contract

--- uncompiledcontracts/AirTrace.sol
+++ uncompiledcontracts/AirTrace.sol
@@ -5,6 +5,7 @@ contract BaggageTracker {
     
     // Define a structure to hold baggage information
     struct Baggage {
+        //raplace airportCode w/ location 
         string airportCode;
         string location;
         string stage;
@@ -17,20 +18,26 @@ contract BaggageTracker {
     // Function to store baggage info
     function storeBaggage(uint256 baggageId, string memory airportCode, string memory location) public {
         baggages[baggageId] = Baggage(airportCode, location);
+        //ADD CODE - BAGGAGES also need stage updated 
     }
 
     function updateLocation(uint256 baggageId, string memory newLocation) public {
     //If ID dosen't exist
     require(bytes(baggages[baggageId].airportCode).length != 0, "Error: Baggage ID does not have a valid airport code");
     require(bytes(baggages[baggageId].location).length != 0, "Error: Baggage ID does not have a valid location");
+    // baggagges[id].stage = newstage
+    //bagagges[id].airportcode = new airportcode 
     baggages[baggageId].location = newLocation;
     }
 
     // Function to view baggage info
+    // VIEW STAGE, 
     function getBaggage(uint256 baggageId) public view returns (string memory location) {
         require(bytes(baggages[baggageId].airportCode).length != 0, "Error: Baggage ID does not have a valid airport code");
         require(bytes(baggages[baggageId].location).length != 0, "Error: Baggage ID does not have a valid location");
         Baggage memory baggage = baggages[baggageId];
         return (baggage.location);
     }
+
+    // function baggages[id].completed => true 
 }
